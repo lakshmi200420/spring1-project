941b1c441720d4326da62037b647889fd1b8cb04
lakshmi200420 <lakshmiramesh202004@gmail.com>
2024-09-02 14:35:40 +0530

    Add files via upload

---
+++ UserService_Lakshmi/src/main/java/com/mvit/userservice/UserController.java
@@ -0,0 +1,28 @@
+package com.mvit.userservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @GetMapping("/{username}")
+    public User getUserByUsername(@PathVariable String username) {
+        return (User) userService.findUserByUsername(username).orElse(null);
+    }
+}
+
---
+++ UserService_Lakshmi/src/main/java/com/mvit/userservice/UserService.java
@@ -0,0 +1,27 @@
+package com.mvit.userservice;
+
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public Optional<User> findUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+}
